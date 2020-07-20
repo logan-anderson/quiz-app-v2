3d2707d94ba7c0d8a8b5129c7aa57eaff15b5679
logan-anderson <landerson2@upei.ca>
2020-07-19 22:12:55 -0300

    update

--- postcss.config.js
+++ postcss.config.js
@@ -3,33 +3,84 @@ class TailwindExtractor {
         return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
     }
 }
+const tailwind = require('tailwindcss')
+
+const IS_DEV = process.env.NODE_ENV === 'production'
+
+const plugins = [tailwind]
 
 
-const plugins = [
-    require('tailwindcss'),
-    process.env.NODE_ENV === 'production'
-        ? require('postcss-purgecss')({
-            content: ['./src/index.html'],
-            extractors: [
-                {
-                    extractor: TailwindExtractor,
-                    extensions: ["html", "js"]
-                }
-            ]
-        })
-        : function () {
-            return []
-        }
-]
-
-if (process.NODE_ENV === 'production') {
-    plugins.push(require('postcss-import'),
+module.exports = {
+    plugins: [
+        require('postcss-import'),
         require('postcss-nested'),
         require('postcss-preset-env')({
             browsers: ['>0.25%', 'not ie 11', 'not op_mini all']
-        }))
+        }),
+        require('tailwindcss'),
+        process.env.NODE_ENV === 'production'
+            ? require('postcss-purgecss')({
+                content: ['./index.html'],
+                extractors: [
+                    {
+                        extractor: TailwindExtractor,
+                        extensions: ["html", "js"]
+                    }
+                ]
+            })
+            : function () {
+                return []
+            }
+    ]
 }
 
-module.exports = {
-    plugins,
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class TailwindExtractor {
+//     static extract(content) {
+//         return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+//     }
+// }
+
+
+// const plugins = [
+//     require('tailwindcss'),
+//     process.env.NODE_ENV === 'production'
+//         ? require('postcss-purgecss')({
+//             content: ['./src/index.html'],
+//             extractors: [
+//                 {
+//                     extractor: TailwindExtractor,
+//                     extensions: ["html", "js"]
+//                 }
+//             ]
+//         })
+//         : function () {
+//             return []
+//         }
+// ]
+
+// if (process.NODE_ENV === 'production') {
+//     plugins.push(require('postcss-import'),
+//         require('postcss-nested'),
+//         require('postcss-preset-env')({
+//             browsers: ['>0.25%', 'not ie 11', 'not op_mini all']
+//         }))
+// }
+
+// module.exports = {
+//     plugins,
+// }
