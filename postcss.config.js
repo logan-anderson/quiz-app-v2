15c2cc220d48479cb12822b37cf7de910a9c7ab1
logan-anderson <landerson2@upei.ca>
2020-07-19 21:04:03 -0300

    update

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,26 @@ const IS_DEV = process.env.NODE_ENV === 'production'
 const plugins = [tailwind]
 
 if (!IS_DEV) {
+    const purgecss = require('@fullhuman/postcss-purgecss')
 
+    class TailWindExtractor {
+        static extract(context) {
+            return context.match(/[A-z0-9-:\/]+/g) || [];
+        }
+    }
+    plugins.push(
+        purgecss({
+            content: ['src/*.html', 'src/*.js', 'src/*.tsx'],
+            extractors: [
+                {
+                    extractor: TailWindExtractor,
+                    extensions: ['html', 'js']
+                }
+            ]
+
+        })
+
+    )
 }
 
 module.exports = {
