a3ca1556936c728fae5adfdc083749f5d766c2da
logan-anderson <landerson2@upei.ca>
2020-07-19 22:09:03 -0300

    update

--- postcss.config.js
+++ postcss.config.js
@@ -3,33 +3,33 @@ class TailwindExtractor {
         return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
     }
 }
-const tailwind = require('tailwindcss')
 
-const IS_DEV = process.env.NODE_ENV === 'production'
 
-const plugins = [tailwind]
+const plugins = [
+    require('tailwindcss'),
+    process.env.NODE_ENV === 'production'
+        ? require('postcss-purgecss')({
+            content: ['./src/index.html'],
+            extractors: [
+                {
+                    extractor: TailwindExtractor,
+                    extensions: ["html", "js"]
+                }
+            ]
+        })
+        : function () {
+            return []
+        }
+]
 
+if (process.NODE_ENV === 'production') {
+    plugins.push(require('postcss-import'),
+        require('postcss-nested'),
+        require('postcss-preset-env')({
+            browsers: ['>0.25%', 'not ie 11', 'not op_mini all']
+        }))
+}
 
 module.exports = {
-    plugins: [
-        // require('postcss-import'),
-        // require('postcss-nested'),
-        // require('postcss-preset-env')({
-        //     browsers: ['>0.25%', 'not ie 11', 'not op_mini all']
-        // }),
-        require('tailwindcss'),
-        process.env.NODE_ENV === 'production'
-            ? require('postcss-purgecss')({
-                content: ['./src/index.html'],
-                extractors: [
-                    {
-                        extractor: TailwindExtractor,
-                        extensions: ["html", "js"]
-                    }
-                ]
-            })
-            : function () {
-                return []
-            }
-    ]
+    plugins,
 }
