915f025b2f62faac8e3bb6b0a2f97ce0e03ae921
logan-anderson <landerson2@upei.ca>
2020-07-19 22:43:00 -0300

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,38 +1,38 @@
-class TailwindExtractor {
-    static extract(content) {
-        return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
-    }
-}
+// class TailwindExtractor {
+//     static extract(content) {
+//         return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+//     }
+// }
 const tailwind = require('tailwindcss')
 
-const IS_DEV = process.env.NODE_ENV === 'production'
+// const IS_DEV = process.env.NODE_ENV === 'production'
 
 const plugins = [tailwind]
 
 
-module.exports = {
-    plugins: [
-        require('postcss-import'),
-        require('postcss-nested'),
-        require('postcss-preset-env')({
-            browsers: ['>0.25%', 'not ie 11', 'not op_mini all']
-        }),
-        require('tailwindcss'),
-        process.env.NODE_ENV === 'production'
-            ? require('postcss-purgecss')({
-                content: ['./index.html'],
-                extractors: [
-                    {
-                        extractor: TailwindExtractor,
-                        extensions: ["html", "js"]
-                    }
-                ]
-            })
-            : function () {
-                return []
-            }
-    ]
-}
+// module.exports = {
+//     plugins: [
+//         require('postcss-import'),
+//         require('postcss-nested'),
+//         require('postcss-preset-env')({
+//             browsers: ['>0.25%', 'not ie 11', 'not op_mini all']
+//         }),
+//         require('tailwindcss'),
+//         process.env.NODE_ENV === 'production'
+//             ? require('postcss-purgecss')({
+//                 content: ['./index.html'],
+//                 extractors: [
+//                     {
+//                         extractor: TailwindExtractor,
+//                         extensions: ["html", "js"]
+//                     }
+//                 ]
+//             })
+//             : function () {
+//                 return []
+//             }
+//     ]
+// }
 
 
 
@@ -60,11 +60,11 @@ module.exports = {
 //     require('tailwindcss'),
 //     process.env.NODE_ENV === 'production'
 //         ? require('postcss-purgecss')({
-//             content: ['./src/index.html'],
+//             content: ['./src/index.html', './src/*', './src/**/*'],
 //             extractors: [
 //                 {
 //                     extractor: TailwindExtractor,
-//                     extensions: ["html", "js"]
+//                     extensions: ["html", "js", "tsx"]
 //                 }
 //             ]
 //         })
@@ -73,14 +73,30 @@ module.exports = {
 //         }
 // ]
 
-// if (process.NODE_ENV === 'production') {
-//     plugins.push(require('postcss-import'),
+// // if (process.NODE_ENV === 'production') {
+//     plugins.push(
+//         require('postcss-import'),
 //         require('postcss-nested'),
 //         require('postcss-preset-env')({
 //             browsers: ['>0.25%', 'not ie 11', 'not op_mini all']
 //         }))
 // }
 
+module.exports = {
+    plugins,
+}
+
+
+// const purgecss = require('@fullhuman/postcss-purgecss');
+
 // module.exports = {
-//     plugins,
-// }
+//   style: {
+//     postcss: {
+//       plugins: [
+//         purgecss({
+//           content: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.ts'],
+//         }),
+//       ],
+//     },
+//   },
+// };
